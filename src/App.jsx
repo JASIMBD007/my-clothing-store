260da86f3d6eed2e10c633a87dc21664fa29bddb
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-08-26 21:30:13 +0200

    navbar component and style added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Directory from './components/Directory/Directory';
+import Home from './Pages/Home/Home';
+import Navbar from './Pages/Navbar/Navbar';
+import Shop from './Pages/Shop/Shop';
 
 function App () {
   return (
-    <Directory />
+    <>
+      <Navbar />
+      <Directory />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/shop' element={<Shop />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Pages/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from 'react'
+import { Link, Outlet } from 'react-router-dom'
+import crown from '../../assets/crown.svg';
+import './Navbar.scss'
+
+const Navbar = () => {
+    return (
+        <Fragment>
+            <div className='navigation'>
+                <Link className='logo-container' to="/">
+                    <img src={crown} className="logo" alt="logo" />
+
+                </Link>
+                <div className='nav-links-container'>
+                    <Link className='nav-link' to="/shop">
+                        SHOP
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    )
+}
+
+export default Navbar
