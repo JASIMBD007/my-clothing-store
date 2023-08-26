d80c38a43f7621af96dae98bf0a3b9204f33ff66
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-08-26 16:39:17 +0200

    error fixed and code structure changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
 import './App.css'
-import Categories from './components/Categories/Categories'
+import './Categories.scss'
 
 function App () {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Women',
+    },
+    {
+      id: 5,
+      title: 'Men',
+    },
+  ]
 
   return (
-    <div>
-      <Categories />
+
+    <div className='categories-container'>
+      {
+        categories.map(({ title, id }) => (
+          <div key={id} className='category-container'>
+            <div className='background-image'></div>
+            <div className='category-body-container'>
+              <h2>{title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+
+        ))
+      }
     </div>
+
   )
 }
 
